0e1d9d4fb9ebe526a820b37ea1f9163a9b0f3254
NataDymerka <@N_A_D@ukr.net>
2019-07-06 23:57:46 +0300

    comments are added

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,15 +1,16 @@
-let a1 =parseInt(window.prompt('Please, enter a1', 1),10);
-let a2 =parseInt(window.prompt('Please, enter a2', 1),10);
-let b1 =parseInt(window.prompt('Please, enter b1', 1),10);
-let b2 =parseInt(window.prompt('Please, enter b2', 1),10);
-let c1 =parseInt(window.prompt('Please, enter c1', 1),10);
-let c2 =parseInt(window.prompt('Please, enter c2', 1),10);
+let a1 = parseInt(window.prompt('Please, enter a1', 1),10);
+let a2 = parseInt(window.prompt('Please, enter a2', 1),10);
+let b1 = parseInt(window.prompt('Please, enter b1', 1),10);
+let b2 = parseInt(window.prompt('Please, enter b2', 1),10);
+let c1 = parseInt(window.prompt('Please, enter c1', 1),10);
+let c2 = parseInt(window.prompt('Please, enter c2', 1),10);
 const div = 2;
+// calculating of the middle of the line AB
 let C1 = (a1 + b1) / div;
 let C2 = (a2 + b2) / div;
-let line = a1 !== b1 || a2 !== b2;
-if (c1 === C1 && c2 === C2 && line) {
-    console.log('true');
+let line = a1 !== b1 || a2 !== b2; // if A and B are not same point, then AB is line
+if (c1 === C1 && c2 === C2 && line) { //comparing of the inputed values of C point with calculated one while AB is line
+    console.log('true'); // C point is the middle of the line
 } else {
     console.log('false');
 }
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,10 +1,10 @@
-let a = parseInt(window.prompt('Please, input first triangle side length', 1), 10);
-let b = parseInt(window.prompt('Please, input second triangle side length', 1), 10);
-let c = parseInt(window.prompt('Please, input third triangle side length', 1), 10);
-if (a < b + c && b < a + c && c < a + b && a > 0 && b > 0 && c > 0) {
-    if (a === b && b === c) {
+let a = parseInt(prompt('Please, input first triangle side length', 1), 10);
+let b = parseInt(prompt('Please, input second triangle side length', 1), 10);
+let c = parseInt(prompt('Please, input third triangle side length', 1), 10);
+if (a < b + c && b < a + c && c < a + b && a > 0 && b > 0 && c > 0) { //checking if the triangle exists
+    if (a === b && b === c) { // checking if all sides are equal
         console.log('Equivalent triangle');
-    } else if (a === b || b === c || a === c) {
+    } else if (a === b || b === c || a === c) { //checking if two sides are equal
         console.log('Isosceles triangle');
     } else {
         console.log('Normal triangle');
